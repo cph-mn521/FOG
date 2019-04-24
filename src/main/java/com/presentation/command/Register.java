802118b0d6db5757c854cc44f20d7e4c272804e7
nfmbang <43202392+nfmbang@users.noreply.github.com>
2019-04-24 10:41:28 +0200

    Revert "Update Register.java"
    This reverts commit b52a76065bae8bfd523174ba39d48b53ebeafcea.

---
+++ src/main/java/com/presentation/command/Register.java
@@ -0,0 +1,28 @@
+package com.presentation.command;
+
+import com.logic.LogicFacade;
+import com.exceptions.LoginException;
+import com.entities.dto.User;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import com.presentation.frontcontrol.FrontController;
+
+public class Register extends Command {
+
+    @Override
+    String execute(HttpServletRequest request, HttpServletResponse response) throws LoginSampleException {
+        String email = request.getParameter("email");
+        String password1 = request.getParameter("password1");
+        String password2 = request.getParameter("password2");
+        if (password1.equals(password2)) {
+            User user = FrontController.createUser(email, password1);
+            HttpSession session = request.getSession();
+            session.setAttribute("user", user);
+            session.setAttribute("role", user.role);
+            return user.role + "page";
+        } else {
+            throw new LoginException("the two passwords did not match.");
+        }
+    }
+}
