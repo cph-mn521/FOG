945968ee62d7320c3f7728ea7d1f52a77982991d
MartinBrandstrup <martin.l.brandstrup@gmail.com>
2019-04-25 15:11:40 +0200

    Create BOMCalculator.java

---
+++ src/main/java/com/logic/BOMCalculator.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.logic;
+
+/**
+ *
+ * @author 69lem
+ */
+public class BOMCalculator
+{
+    
+}
