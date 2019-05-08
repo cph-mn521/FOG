426f3572f17bfe9db083dd854fb603eb6b090312
cph-mn521 <43202726+cph-mn521@users.noreply.github.com>
2019-05-08 13:24:12 +0200

    erh. det her var ikk til develop.

--- src/main/webapp/js/AJAXTEST.js
+++ src/main/webapp/js/AJAXTEST.js
@@ -12,8 +12,14 @@ function TEST() {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       document.getElementById("test").innerHTML = this.responseText;
+      document.getElementById("buybutton").addEventListener("click", funk());
     }
   };
   xhttp.open("GET", "FrontController?command=JSTEST", true);
   xhttp.send();
+}
+
+
+function funk(){
+    document.getElementById("buybox").innerHTML = "du har købt en karport.. yaaay."
 }
