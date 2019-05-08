e794c058315d66dfd569f1151ecb4bbb91283486
Martin Sander-Thomsen <cph-ms782@cphbusiness.dk>
2019-05-08 21:04:59 +0200

    AJAX next step

--- src/main/webapp/js/sideNavScript.js
+++ src/main/webapp/js/sideNavScript.js
@@ -5,3 +5,16 @@ function toggleContent(tagID)
     $('#content #contentContainer ' + tagID).removeClass('hiding');
     $('#content #contentContainer ' + tagID).addClass('showing');
 }
+
+function showOrders() {
+  
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("test").innerHTML = this.responseText;
+//      document.getElementById("buybutton").addEventListener("click", funk());
+    }
+  };
+  xhttp.open("GET", "FrontController?command=ShowOrders", true);
+  xhttp.send();
+}
