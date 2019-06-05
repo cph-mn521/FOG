d204cd57d76e6a60146221cb7fd30a188d0fd584
cph-mn521 <43202726+cph-mn521@users.noreply.github.com>
2019-06-05 14:23:48 +0200

    Removed un-used import in command

--- src/main/java/com/presentation/command/Command.java
+++ src/main/java/com/presentation/command/Command.java
@@ -1,7 +1,6 @@
 package com.presentation.command;
 
 import java.util.HashMap;
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
