7d73c89c3c8a6f4150ef32933b593e7e519d6ee2
nfmbang <43202392+nfmbang@users.noreply.github.com>
2019-06-07 14:38:14 +0200

    Ryddet op i imports

--- src/main/java/com/data/PDF/PDFGenerator.java
+++ src/main/java/com/data/PDF/PDFGenerator.java
@@ -5,38 +5,27 @@
  */
 package com.data.PDF;
 
-import com.entities.dto.Component;
 import com.entities.dto.Customer;
 import com.entities.dto.Order;
-import com.exceptions.LogicException;
 import com.exceptions.PDFException;
-import com.itextpdf.text.BadElementException;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
-import com.logic.MappingLogic;
 import com.logic.PDFCalculator;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.sql.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author Brandstrup
  */
-public class PDFGenerator
-{
-    
+public class PDFGenerator {
+
     private final PDFCalculator pdfcalc = new PDFCalculator();
-    
+
     //Temp main method for testing purposes
 //    public static void main(String[] args)
 //    {
@@ -44,24 +33,24 @@ public class PDFGenerator
 //        Random rand = new Random();
 //
 //        int amountOfEntries = 20; //amount of entries to generate the test file with
-//        
+//
 //        String[] desc = new String[5];
 //        String[] helpt = new String[5];
-//        
+//
 //        desc[0] = "Røde vingetagsten";
 //        desc[1] = "Cembrit tagskrue sortblå 120mm m/skive";
 //        desc[2] = "38x73 mm. taglægte T1";
 //        desc[3] = "97x97 mm. trykimp. Stolpe";
 //        desc[4] = "5,0 x 40 mm. beslagskruer 250 stk.";
-//        
+//
 //        helpt[0] = "Gl. dansk forbrug: 14,6 stk/m2 - 6 stk/bdt - 144 stk/½pal. - 288 pr pal. lægteafstand: 325mm dækbredde 201";
 //        helpt[1] = "Cembrit sortblå tagskrue med skrive, til montering af B6, B7 og B9 bølgeplader";
 //        helpt[2] = "Til montering på spær, 7 rækker lægter på hver skiftevis 1 hel & 1 halv lægte";
 //        helpt[3] = "Til montering på spær";
 //        helpt[4] = "Til montering af universalbeslag + toplægte";
-//        
+//
 //        float totalPrice = 0;
-//        
+//
 //        for (int i = 0; i < amountOfEntries; i++)
 //        {
 //            int dIndex = rand.nextInt(desc.length);
@@ -82,11 +71,11 @@ public class PDFGenerator
 //        String filePath = "src/main/resources/pdf/";
 //        String title = "Stykliste";
 //        String headerTitle = "Stykliste for Carport";
-//        
+//
 //        Customer customer = new Customer(1, "bittie_bertha", "bertha@testmail.com", "1234", "26154895");
 //        Order order = new Order(1, 1, Date.valueOf("2019-04-03"),
 //                Date.valueOf("2019-04-14"), "Fantasivej 12 Lyngby", "sent", totalPrice);
-//        
+//
 //
 //        try
 //        {
@@ -97,7 +86,6 @@ public class PDFGenerator
 //        {
 //        }
 //    }
-    
     /**
      * The method to initialize the generation of the PDF document. Employs
      * several methodsfrom Logic.PDFCalculator to generate each section of the
@@ -115,42 +103,34 @@ public class PDFGenerator
      * @throws PDFException if an error occurs during the generation of the PDF
      * @author Brandstrup
      */
-    public void generatePDFFromBill(java.util.List<String> BOMStringList, 
-            String author, String fileName, String filePath, String title, 
-            String headerTitle, Customer customer, Order order) throws PDFException
-    {
+    public void generatePDFFromBill(java.util.List<String> BOMStringList,
+            String author, String fileName, String filePath, String title,
+            String headerTitle, Customer customer, Order order) throws PDFException {
         File file = new File(filePath + fileName + ".pdf");
         Document document = new Document();
-        
-        try
-        {
+
+        try {
             PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream(file));
             HeaderFooterPageEvent event = new HeaderFooterPageEvent();
             writer.setPageEvent(event);
             document.open();
-            
+
             pdfcalc.addMetaData(document, title);
 
             Paragraph frontpage = pdfcalc.addFrontPageInfo(author, headerTitle, customer, order);
             document.add(frontpage);
-            
+
             document.newPage();
-            
+
             Paragraph bill = pdfcalc.addBillTable(BOMStringList);
             document.add(bill);
 
             document.close();
-        }
-        catch (FileNotFoundException ex)
-        {
+        } catch (FileNotFoundException ex) {
             throw new PDFException("Fejl i generatePDFFromBill. FileNotFoundException");
-        }
-        catch (IOException ex)
-        {
+        } catch (IOException ex) {
             throw new PDFException("Fejl i generatePDFFromBill. IOException");
-        }
-        catch (DocumentException ex)
-        {
+        } catch (DocumentException ex) {
             throw new PDFException("Fejl i generatePDFFromBill. DocumentException");
         }
     }
